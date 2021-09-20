e9b3058a6df3df644aabaa90638e8d7900e2b893
Joaquim Perez <jivkim@gmail.com>
2021-09-20 23:51:15 +0200

    styling

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@
 </template>
 
 <style lang="scss">
+body {
+  background-color: gainsboro;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,10 +24,17 @@
 
 #nav {
   padding: 30px;
+  max-width: 80vw;
+  width: 300px;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+    font-weight: bold;
 
     &.router-link-exact-active {
       color: #42b983;
