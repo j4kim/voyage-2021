de428a6ec9bb8805bdba56b45764f524766fefe3
Joaquim Perez <jivkim@gmail.com>
2021-09-22 00:04:02 +0200

    fetch markdown content and parse each post splitting by --- and retrieving id from comment using a regex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import marked from 'marked'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+
+  data: {
+    content: ''
+  },
+
+  computed: {
+    rawPosts() {
+      return this.content.split('---')
+    },
+    posts() {
+      return this.rawPosts.map(md => {
+        let id = md.match(/(?!<!-- *id: *)\d+(?= *-->)/)[0]
+        return { id, html: marked(md) }
+      })
+    }
+  },
+
+  created() {
+    fetch('content.md')
+      .then(response => response.text())
+      .then(text => this.content = text)
+  }
 }).$mount('#app')
